449697eb46431bdeeb6b068cf8144eea4fd266f9
kenkbomb <146538181+kenkbomb@users.noreply.github.com>
2023-10-06 01:02:00 -0400

    started task 1.2
    started task 1.2, added a pokemon list and some pokemon objects and pushed them into the list then used document.write to list the objects and their properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-alert('Hello World!');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList=[];
+let types = ['Electric','Water','Fire','Ground','Rock','Normal','Grass','Bug','Flying','Poison','Fairy','Fighting','Pyschic','Steel','ghost'];
+
+let Pikachu = {name:'Pikachu',type:types[0],height:0.4};
+let Charmander = {name:'Charmander',type:types[2],height:0.6};
+let Squirtle = {name:'Squirtle',type:types[1],height:0.5};
+let Ditto = {name:'Ditto',type:types[5],height:0.3};
+let Diglett = {name:'Diglett',type:types[3],height:0.2};
+
+pokemonList.push(Pikachu);
+pokemonList.push(Charmander);
+pokemonList.push(Squirtle);
+pokemonList.push(Diglett);
+pokemonList.push(Ditto);
+
+for(let i=0;i<pokemonList.length;i++)
+{
+    document.write("Name: " + pokemonList[i].name + ", Type: " + pokemonList[i].type + ", Height: " + pokemonList[i].height +'M' + '<br>');
+}
+
