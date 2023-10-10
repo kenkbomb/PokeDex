2e98cae2faf915f13973f74f8efab814f81a2d70
kenkbomb <146538181+kenkbomb@users.noreply.github.com>
2023-10-10 12:51:43 -0400

    task 1.6 code and logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@
 {name:'Charizord',types:['Flying','Fire'],height:1.7},
 {name:'Diglett',types:['Ground'],height:0.2}
 ];
-*/
+
 function listPokeMonWithFor(){
     for(let i=0;i<pokemonList.length;i++)
     {
@@ -35,40 +35,72 @@ function listPokeMonWithForEach()
     document.write('<br> </p>');
     })
 }
-
+*/
 function listPokeMonWithIIFE()
 {
-   
+    function addListItem(pokemon)
+    {
+            let pokeItem = document.createElement('li');
+            let pokeButton = document.createElement('button');
+            pokeButton.innerText = pokemon.name;
+            pokeButton.classList.add('pokeButton');
+            pokeItem.appendChild(pokeButton);
+            htmlPokeList.appendChild(pokeItem);
+            pokeButton.addEventListener('click',function(event)
+            {
+                showDetails(pokemon);
+                // alert(pokemon.name+ " " + pokemon.types+ " " + pokemon.height);
+            })
+    }
+
+    function showDetails(pokemon)
+    {
+        console.log(pokemon);
+    }
     let pokelist = pokeIIFe.getAll();
+    let htmlPokeList = document.querySelector('ul');
 
     pokelist.forEach(function(pokemon)
     {
-        document.write('<p>' + 'Name: ' + pokemon.name + ', Types: ('+ pokemon.types + '), Height: '+ pokemon.height);
+        addListItem(pokemon);
+
+
+      /*  document.write('<p>' + 'Name: ' + pokemon.name + ', Types: ('+ pokemon.types + '), Height: '+ pokemon.height);
 
     if(pokemon.height>1.6)
     {
         document.write('   ...WOW! that is big!');
     }
     
-    document.write('<br> </p>');
+    document.write('<br> </p>');*/
     })
 }
 
 let pokeIIFe = (function()
 {
+
+
     let pokemonList=[
         {name:'Pikachu',types: ['Electric'],height:0.4},
         {name:'Charmander',types:['Fire'],height:0.6},
         {name:'Blastoise',types:'Water',height:1.6},
         {name:'Squirtle',types:['Water'],height:0.5},
         {name:'Charizord',types:['Flying','Fire'],height:1.7},
-        {name:'Diglett',types:['Ground'],height:0.2}
+        {name:'Diglett',types:['Ground'],height:0.2},
+       { name:'Metapod',types:['bug'],height:0.7}
+
         ];
 
      return{
         add:function(pokemon)
         {
+            if(pokemon instanceof Object)
+            {
             pokemonList.push(pokemon);
+            }
+            else {
+                alert('Pokemon only!');
+            }
         },
         getAll:function()
         {
@@ -80,10 +112,11 @@ let pokeIIFe = (function()
 })();
 
 // listPokeMonWithForEach();
-let pokemon  = {
+/*let pokemon  = {
     name:'Metapod',types:['bug'],height:0.7
 }
 pokeIIFe.add(pokemon);
+pokeIIFe.add(6);*/
 listPokeMonWithIIFE();
 
 
